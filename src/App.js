720cdaadaea025d6d8ad5ddf31ba52df0e8ed71f
mohi-uddin-bff <mohiuddin6948@gmail.com>
2020-08-24 10:07:51 +0600

    add JavaScript html css in react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [ users, setUser] = useState([]);
+  useEffect(() => {
+    fetch("https://randomuser.me/api/")
+    .then(res => res.json())
+    .then(data => setUser(data.results))
+  }, [])
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          users.map(user => <RandomUser properties={user}></RandomUser>)
+        }
       </header>
     </div>
   );
+}
+
+function RandomUser(props) {
+  const { name, location, email, picture } = props.properties;
+  console.log(picture);
+  const userImage = {
+    position: "relative",
+    background: "#fff",
+    padding: "5px",
+    width: "150px",
+    height: "150px",
+    borderRadius: "50%",
+    marginBottom: "30px",
+    border: "1px solid rgba(0,0,0,.25)",
+    left: "50%",
+    transform: "translateX(-50%)"
+  }
+  const liStyle = {
+    display: "block",
+    width: "40px",
+    height: "48px",
+    float: "left",
+    margin: "20px",
+  }
+  return (
+    <div>
+      <div style={userImage}>
+        <img style={{width: "150px", borderRadius: "50%"}} src={picture.large}/>
+      </div>
+      <p>Hi, My name is</p>
+      <h3>{name.title} {name.first} {name.last}</h3>
+      <p><small>{location.state}, {location.city}</small></p>
+      <p><small>Email: {email}</small></p>
+    </div>
+  )
 }
 
 export default App;
